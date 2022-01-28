a42c7df233f5834d2e103a45047c3174715e7f99
eudora <contact@doraeu.net>
2022-01-28 21:04:56 +0900

    ユーザーCRU実装 #6

--- main.py
+++ main.py
@@ -23,12 +23,13 @@ def int_to_time(number):
     time = basetime + timedelta(seconds=number)
     return time
 
+
 @bot.slash_command(guild_ids=[929200570093424660], name="list")
 async def list_command(ctx):
     if not data_ready:
         return await ctx.respond(f'申し訳ございません。再起動の処理中です。しばらくお待ちしてからもう一度試してください。', ephemeral=True)
     
-    data = await timers_collection.find(
+    list_data = await timers_collection.find(
         {
             "guild_id": ctx.guild.id,
             "author_id": ctx.author.id,
@@ -37,13 +38,13 @@ async def list_command(ctx):
         }
     ).to_list(length=None)
 
-    data = sorted(data, key=lambda x:x['datetime'])
+    sorted_list_data = sorted(data, key=lambda x:x['datetime'])
 
     text = ""
-    for i, item in enumerate(data):
+    for i, item in enumerate(sorted_list_data):
         text += f"{i}) {int_to_time(item['datetime'])}: {item['message']}"+"\n"
 
-    if not data:
+    if not list_data:
         text = "設定したタイマーはありません。"
 
     embed = discord.Embed(
@@ -53,36 +54,31 @@ async def list_command(ctx):
     )
     await ctx.respond(embed=embed)
 
-
 @bot.slash_command(guild_ids=[929200570093424660], name="delete")
 async def delete_command(
     ctx,
-    sec: Option(int, '通知する秒数'),
-    message: Option(str, '通知するときのコマンドを入力', required=False, default='メッセージなし'),
+    select: Option(int, '削除するタイマーの番号'),
 ):
     if not data_ready:
         return await ctx.respond(f'申し訳ございません。再起動の処理中です。しばらくお待ちしてからもう一度試してください。', ephemeral=True)
 
-    await ctx.respond(f'{sec}秒後に通知します。', ephemeral=False)
-    time = datetime.now() + timedelta(seconds=sec)
-    time = time_to_int(time)
-    data.append({
-        'guild_id': ctx.guild.id,
-        'channel_id': ctx.channel.id,
-        'author_id': ctx.author.id,
-        'datetime': time,
-        'message': message,
-    })
+    list_data = await timers_collection.find(
+        {
+            "guild_id": ctx.guild.id,
+            "author_id": ctx.author.id,
+        }, {
+            "_id": False
+        }
+    ).to_list(length=None)
 
-    await timers_collection.insert_one({
-        'guild_id': ctx.guild.id,
-        'channel_id': ctx.channel.id,
-        'author_id': ctx.author.id,
-        'datetime': time,
-        'message': message,
-    })
+    sorted_list_data = sorted(data, key=lambda x:x['datetime'])
 
+    if not (0 <= select < len(sorted_list_data)):
+        return await ctx.respond("不正な値です。", ephemeral=True)
 
+    await timers_collection.delete_one(sorted_list_data[select])
+    data.remove(sorted_list_data[select])
+    return await ctx.respond("削除が完了しました。", ephemeral=False)
 
 @bot.slash_command(guild_ids=[929200570093424660], name="set")
 async def set_command(
@@ -139,7 +135,12 @@ async def loop():
 @bot.event
 async def on_ready():
     global data_ready, data
-    data = await timers_collection.find().to_list(length=None)
+    data = await timers_collection.find(
+        {},
+        {
+            "_id": False
+        }
+    ).to_list(length=None)
     data_ready = True
 
 with open("./config.json", 'r', encoding='utf-8') as f:
